3f4ce04386ecf9cc594350c2de74446756791e0b
Yael Stein <yaelst@gmail.com>
2016-09-07 17:09:47 +0200

    full carousel, no clicks

--- kitty.js
+++ kitty.js
@@ -1,21 +1,44 @@
-setTimeout(function() {
+
+
+(function() {
+
+    var tempDots = document.getElementsByClassName('dots');
+    var dots = [];
+    for (var i=1;i<tempDots.length;i++)
+    {
+        dots.push(tempDots[i]);
+    }
+
     var images = document.getElementsByClassName('pics');
-    images[0].style.transitionProperty = "transform";
-    images[0].style.transitionDuration = "4s";
-    images[0].style.transform = "translateX(-1440px)";
-    images[1].style.transitionProperty = "transform";
-    images[1].style.transitionDuration = "4s";
-    images[1].style.transform = "translateX(-1440px)";
+    dots[0].style.backgroundColor = "white";
+
+
+function general(current) {
+    var next = current+1;
+    if (next===4) {
+        next=0;
+    }
+    if(current===4) {
+        general(0);
+    }
+
+
+        setTimeout (function () {
+            dots[next].style.backgroundColor = "white";
+            images[current].classList.add("middle-to-left");
+            images[next].classList.add("right-to-middle");
+            dots[current].style.backgroundColor = "transparent";
+        }, 1500);
+        setTimeout(function() {
+        images[current].classList.remove("middle-to-left");
+           images[current].style.right = "-1440px";
+           images[next].classList.remove("right-to-middle");
+           images[next].style.right = "0px";
+           general(current+1);
+       }, 4800)
 
 
 
-    //function looping() {
-    //    for (var i=0;i<images.length;i++) {
-    //        images[i].classList.add("go-left");
-    //        images[i+1].classList.add("go-to-middle");
-    //        if(i===pics.length-1) {
-    //            i===0;
-    //        }
-    //    }
-    //}
-},1000);
+}
+general(0);
+})();
